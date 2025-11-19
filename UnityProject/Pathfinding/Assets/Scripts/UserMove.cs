b1147e1895ab55079dca98c3fd1f5726dc209bf1
Enne43 <naiara4rtb@gmail.com>
2025-11-19 20:44:44 +0100

    Queda el movimiento, ahora mismo hay teletransporte.

--- UnityProject/Pathfinding/Assets/Scripts/UserMove.cs
+++ UnityProject/Pathfinding/Assets/Scripts/UserMove.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using static PathfindAgent;
@@ -8,22 +9,38 @@ public class UserMove : MonoBehaviour
     [SerializeField] PathUser pathUser;
     [SerializeField] Button playButton;
     [SerializeField] Button restartButton;
+    [SerializeField] PATHFIND_ALGORITHM algorithm;
+
+    Transform userTransform;
     #endregion
 
     private void OnEnable()
     {
-        //playButton.onClick.AddListener();
-        //restartButton.onClick.AddListener();
+        playButton.onClick.AddListener(()=>MoveUser(algorithm));
+        restartButton.onClick.AddListener(()=>GoBack(algorithm));
     }
+
 
     private void OnDisable()
     {
         playButton.onClick.RemoveAllListeners();
         restartButton.onClick.RemoveAllListeners();
     }
+    private void Start()
+    {
+        userTransform = GetComponent<Transform>();
+    }
 
     public void MoveUser(PATHFIND_ALGORITHM algorithm)
     {
-        pathUser.GetPathWithAlgorithm(algorithm);
+        Path userPath = pathUser.GetPathWithAlgorithm(algorithm);
+        foreach (PathNode node in userPath.pathNodes) 
+        {
+            userTransform.position = node.transform.position; // EVITAR EL TELETRANSPORTE, PERO LA LISTA ES ESA
+        }
+    }
+    private void GoBack(PATHFIND_ALGORITHM algorithm)
+    {
+        
     }
 }
