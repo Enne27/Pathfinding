d7add7e81c407b5083814bea34d3e090ffdc920f
Enne <nsamosag@gmail.com>
2025-11-20 14:01:18 +0100

    Movement finished. It remains clearing and comenting code.
    And presentation.

--- UnityProject/Pathfinding/Assets/Scripts/UserMove.cs
+++ UnityProject/Pathfinding/Assets/Scripts/UserMove.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 using static PathfindAgent;
 
@@ -8,39 +12,93 @@ public class UserMove : MonoBehaviour
     #region VARIABLES
     [SerializeField] PathUser pathUser;
     [SerializeField] Button playButton;
-    [SerializeField] Button restartButton;
+    [SerializeField, FormerlySerializedAs("restartButton")] Button goBackButton;
+    [SerializeField] Button restartPositionButton;
     [SerializeField] PATHFIND_ALGORITHM algorithm;
 
+    [SerializeField] float speed = 2f;
+
     Transform userTransform;
+    Coroutine moveCoroutine;
+
+    List<PathNode> visitedNodes = new List<PathNode>();
+    Vector3 originalPos;
+    bool goingBack = false;
     #endregion
 
     private void OnEnable()
     {
-        playButton.onClick.AddListener(()=>MoveUser(algorithm));
-        restartButton.onClick.AddListener(()=>GoBack(algorithm));
+        playButton.onClick.AddListener(()=>StartUserMovement(algorithm));
+        goBackButton.onClick.AddListener(()=>GoBack(algorithm));
+        restartPositionButton.onClick.AddListener(ReturnToOriginalPos);
     }
-
 
     private void OnDisable()
     {
         playButton.onClick.RemoveAllListeners();
-        restartButton.onClick.RemoveAllListeners();
+        goBackButton.onClick.RemoveAllListeners();
+        restartPositionButton.onClick.RemoveAllListeners();
     }
     private void Start()
     {
         userTransform = GetComponent<Transform>();
+        originalPos = userTransform.position;
     }
 
-    public void MoveUser(PATHFIND_ALGORITHM algorithm)
+    void ReturnToOriginalPos() 
+    { 
+        transform.position = originalPos; 
+        visitedNodes.Clear();
+        goingBack = false;
+    }
+
+    public void StartUserMovement(PATHFIND_ALGORITHM algorithm)
     {
         Path userPath = pathUser.GetPathWithAlgorithm(algorithm);
-        foreach (PathNode node in userPath.pathNodes) 
+        
+        if (userPath != null) 
         {
-            userTransform.position = node.transform.position; // EVITAR EL TELETRANSPORTE, PERO LA LISTA ES ESA
+            goingBack = false;
+            visitedNodes.Clear();
+            if (moveCoroutine != null) StopCoroutine(moveCoroutine);
+            moveCoroutine = StartCoroutine(FollowPath(userPath));
         }
     }
+
+    private IEnumerator FollowPath(Path userPath)
+    {
+        foreach (PathNode node in userPath.pathNodes) 
+        {
+            Vector3 target = node.transform.position;
+            while (Vector3.Distance(userTransform.position, target) > 0.05f)
+            {
+                userTransform.position = Vector3.MoveTowards(
+                    userTransform.position,
+                    target,
+                    speed * Time.deltaTime
+                );
+                yield return null; // Espera 1 frame antes de moverse.
+            }
+            if(!goingBack) 
+                visitedNodes.Add(node);
+        }
+    }
+
     private void GoBack(PATHFIND_ALGORITHM algorithm)
     {
-        
+        if (visitedNodes.Count == 0) return;
+
+        if (moveCoroutine != null) StopCoroutine(moveCoroutine);
+
+        goingBack = true;
+        visitedNodes.RemoveAt(visitedNodes.Count - 1);
+
+        // Path invertido
+        PathNode[] reversedNodes = visitedNodes.ToArray();
+        Array.Reverse(reversedNodes);
+
+        Path reversedPath = new Path(reversedNodes);
+
+        moveCoroutine = StartCoroutine(FollowPath(reversedPath));
     }
 }
